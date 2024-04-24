00a0b86acfca60028d5f8661798d42fe51a1a299
SupiiX <148445324+SupiiX@users.noreply.github.com>
2024-04-24 17:12:13 +0200

    p

--- DataFight/Assets/PlayerScript/Dodge.cs
+++ DataFight/Assets/PlayerScript/Dodge.cs
@@ -8,15 +8,26 @@ public class Dodge : MonoBehaviour
     public float dodgeSpeed = 5f;
 
     // A dodging mértéke (x tengelyen mennyit térünk ki)
-    public float dodgeDistance = 0.5f;
+    public float DodgeTime = 0.5f;
 
     // Az utolsó mozgásirány
     private Vector2 lastMoveDirection = Vector2.zero;
+
+    public bool ISdodging = false;
+
+    // A dodge cooldown ideje
+    public float dodgeCooldown = 1.5f;
+
+    private float dodgeTimer;
+
+    private bool FirstTime = true;
 
     private Rigidbody2D rb;
 
     private void Start()
     {
+        dodgeTimer = dodgeCooldown;
+
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -29,21 +40,20 @@ public class Dodge : MonoBehaviour
         // Frissítjük az utolsó mozgásirányt
         if (horizontalInput != 0f || verticalInput != 0f)
         {
-          //  lastMoveDirection = new Vector2(horizontalInput, verticalInput);
-
-            lastMoveDirection = horizontalInput < 0 ? Vector2.left: Vector2.right;
-
+            // lastMoveDirection = new Vector2(horizontalInput, verticalInput);
+            lastMoveDirection = horizontalInput < 0 ? Vector2.left : Vector2.right;
         }
-    }
+        else if (FirstTime)
+        {
 
-    private void FixedUpdate()
-    {
-        // Ellenõrizzük, hogy a játékos kitér-e
-        if (Input.GetButtonDown("Dodge"))
+            lastMoveDirection = Vector2.right;
+            FirstTime = false;
+        }
+
+        // Ellenõrizzük, hogy a játékos kitér-e és hogy letelt-e a cooldown
+        if (Input.GetButtonDown("Dodge") && dodgeTimer <= 0)
         {
             // Beolvastuk a joystick bevitelt
-            float horizontalInput = Input.GetAxisRaw("Horizontal");
-
             // Meghatározzuk a kitérés irányát
             Vector2 dodgeDirection;
 
@@ -59,19 +69,29 @@ public class Dodge : MonoBehaviour
             {
                 dodgeDirection = lastMoveDirection;
             }
-           
 
             // Kitérés végrehajtása
             rb.velocity = dodgeDirection * dodgeSpeed;
 
-            StartCoroutine(StopDodgeAfterTime(0.2f));
+            ISdodging = true;
+
+            StartCoroutine(StopDodgeAfterTime(DodgeTime));
+
+            // Cooldown beállítása
+            dodgeTimer = dodgeCooldown;
         }
+
+        // Cooldown csökkentése
+        dodgeTimer -= Time.deltaTime;
+
+        Debug.Log($"{dodgeTimer}");
     }
 
     private IEnumerator StopDodgeAfterTime(float dodgeTime)
     {
         yield return new WaitForSeconds(dodgeTime);
         rb.velocity = Vector2.zero;
+        ISdodging = false;
     }
 }
 
