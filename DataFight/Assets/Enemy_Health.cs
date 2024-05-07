5206d29c3958ab09e2b439741d6a98dbff7d18cc
SupiiX <148445324+SupiiX@users.noreply.github.com>
2024-05-07 22:33:51 +0200

    ..

--- DataFight/Assets/Enemy_Health.cs
+++ DataFight/Assets/Enemy_Health.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class Enemy_Health : MonoBehaviour
 {
 
+<<<<<<< Updated upstream
     public int maxHealt = 10;
     [SerializeField] private int currentHealth;
+=======
+    public int maxHealt = 100;
+    private int currentHealth;
+>>>>>>> Stashed changes
     // Start is called before the first frame update
     void Start()
     {
@@ -19,15 +24,32 @@ public class Enemy_Health : MonoBehaviour
         
     }
 
+<<<<<<< Updated upstream
     
+=======
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.tag == "Bullet")
+        {
+            if (currentHealth <= 0)
+            {
+                Destroy(gameObject);
+            }
+            else { TakeDamage(100); }
+        }
+    }
+>>>>>>> Stashed changes
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
+<<<<<<< Updated upstream
         if(currentHealth <= 0)
         {
             Destroy(gameObject);
         }
         Debug.Log($"{amount} adamage taken");
+=======
+>>>>>>> Stashed changes
     }
 }
